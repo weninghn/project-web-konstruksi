fc9e5d7f9017400e51080d1ce5252d7131be676e
febianaayurach <rachmafebiana@gmail.com>
2023-01-24 14:49:42 +0700

    kedua

--- app/Models/Picture.php
+++ app/Models/Picture.php
@@ -5,14 +5,20 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Payment_method extends Model
+class Picture extends Model
 {
     use HasFactory;
-    protected $table = 'payment_methods';
+    protected $table = 'pictures';
     public $timetamps = 'false';
     protected $primaryKey ='id';
-    
+
     protected  $fillable= [
-        'method',
+        'project_id',
+        'image',
     ];
+
+    public function progres()
+    {
+        return $this->belongsTo(Picture::class);
+    }
 }
