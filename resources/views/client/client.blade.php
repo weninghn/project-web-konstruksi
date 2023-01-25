aae62043536bb6f70d791012959469e59af4f17a
auriel170905 <aureliavegayantaaurel@gmail.com>
2023-01-25 11:35:03 +0700

    client

--- resources/views/client/client.blade.php
+++ resources/views/client/client.blade.php
@@ -1,29 +1,6 @@
 @extends('layouts.master')
 
 @section('content')  
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>AdminLTE 3 | DataTables</title>
-
-  <!-- Google Font: Source Sans Pro -->
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-  <!-- Font Awesome -->
-  <link rel="stylesheet" href="../../plugins/fontawesome-free/css/all.min.css">
-  <!-- DataTables -->
-  <link rel="stylesheet" href="../../plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
-  <link rel="stylesheet" href="../../plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
-  <link rel="stylesheet" href="../../plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
-  <!-- Theme style -->
-  <link rel="stylesheet" href="../../dist/css/adminlte.min.css">
-</head>
-<body class="hold-transition sidebar-mini">
-<div class="wrapper">
-
-
   <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
     <!-- Content Header (Page header) -->
@@ -31,7 +8,6 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1>Data Client</h1>
           </div>
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
@@ -46,14 +22,19 @@
     <!-- Main content -->
     <section class="content">
       <div class="container-fluid">
+      <div class="container-wrapper">
         <div class="row">
           <div class="col-12">
             <div class="card">
               <div class="card-header">
-              <a href="/tambahdata" class="btn btn-success">Tambah Data</a>
+              <h3 class="card-title">Data Client</h3>
+                <div class="row justify-content-end">
+              <a href="/tambahdata" class="btn btn-success">+ new</a>
               </div>
-              <!-- /.card-header
-              <div class="card-body"> -->
+              </div>
+              <div class="row g-3 align-items-center mt-2">
+              <!-- .card-header -->
+              <div class="card-body"> 
                 <table id="example2" class="table table-bordered table-hover">
                   <thead>
                   <tr>
@@ -61,15 +42,20 @@
                     <th>Name</th>
                     <th>Phone</th>
                     <th>Address</th>
+                    <th>Aksi</th>
                   </tr>
-                  </thead>
-                  <tbody>
+                  @foreach($data as $row)
                   <tr>
-                    <td>Trident</td>
-                    <td>Internet
-                    <td>Explorer 4.0</td>
-                    <td>Win 95+</td>
+                    <td>{{ $row->id }}</td>
+                    <td>{{ $row->name }}</td>
+                    <td>{{ $row->phone }}</td>
+                    <td>{{ $row->address }}</td>
+                    <td>
+                    <a href="/tampilkandata/{{ $row->id }}" class="btn btn-info">Edit</button>
+                    <a href="#" class="btn btn-danger delete" data-id="{{ $row->id }}">Delete</button>
+                    </td>
                   </tr>
+                  @endforeach
                 </table>
               </div>
               <!-- /.card-body -->
@@ -84,44 +70,6 @@
     </section>
     <!-- /.content -->
   </div>
-  <!-- /.content-wrapper -->
-  <footer class="main-footer">
-    <div class="float-right d-none d-sm-block">
-      <b>Version</b> 3.2.0
-    </div>
-    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
-  </footer>
-
-  <!-- Control Sidebar -->
-  <aside class="control-sidebar control-sidebar-dark">
-    <!-- Control sidebar content goes here -->
-  </aside>
-  <!-- /.control-sidebar -->
-</div>
-<!-- ./wrapper -->
-
-<!-- jQuery -->
-<script src="../../plugins/jquery/jquery.min.js"></script>
-<!-- Bootstrap 4 -->
-<script src="../../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-<!-- DataTables  & Plugins -->
-<script src="../../plugins/datatables/jquery.dataTables.min.js"></script>
-<script src="../../plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
-<script src="../../plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
-<script src="../../plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
-<script src="../../plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
-<script src="../../plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
-<script src="../../plugins/jszip/jszip.min.js"></script>
-<script src="../../plugins/pdfmake/pdfmake.min.js"></script>
-<script src="../../plugins/pdfmake/vfs_fonts.js"></script>
-<script src="../../plugins/datatables-buttons/js/buttons.html5.min.js"></script>
-<script src="../../plugins/datatables-buttons/js/buttons.print.min.js"></script>
-<script src="../../plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
-<!-- AdminLTE App -->
-<script src="../../dist/js/adminlte.min.js"></script>
-<!-- AdminLTE for demo purposes -->
-<script src="../../dist/js/demo.js"></script>
-<!-- Page specific script -->
 <script>
   $(function () {
     $("#example1").DataTable({
