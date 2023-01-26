77060b3d27aad7e55e140d750ca238d07cb24861
weninghn@gmail.com <weninghn@gmail.com>
2023-01-26 14:52:27 +0700

    adduser

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,14 +2,14 @@
 
 namespace App\Http\Controllers;
 use App\Models\Role;
-use App\Models\User;
+use App\Models\Users;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
     public function index(Request $request)
     {
-        $data = User::all();
+        $data = Users::all();
         return view('user.user',['user' => $data]);
     }
 
@@ -18,21 +18,30 @@ class UserController extends Controller
         $role = Role::all();
         return view ('user.adduser',['roles'=> $role]);
     }
-    public function insertuser(Request $request){
-        //dd($request->all());
-         User::create($request->all());
-         return redirect()->route('user')->with('success','Data Berhsail di Tambahkan');
+    public function insertuser(Request $request)
+    {
+        // dd($request->all());
+        $user =[
+            'role_id' => $request->role_id,
+            'name'=> $request->name,
+            'email'=> $request->email,
+            'addres'=> $request->addres,
+            'phone'=> $request->phone,
+            'password' => $request->password,
+        ];
+        Users::create($user);
+        return redirect('user')->with('Success','User Added Successfully'); 
     }
     public function updateuser(Request $request, $id)
     {
-        $user = User::find($id);
+        $user = Users::find($id);
         $user->update($request->all());
         return redirect()->route('user')->with('Success', 'Data berhasil di update');
     }
 
     public function deleteuser($id)
     {
-        $user = User::find($id);
+        $user = Users::find($id);
         $user->delete();
         return redirect()->route('user')->with('Success', 'Data Berhasil Dihapus');
     }
