6f89ba695f5658349a887896a37e712f86920d87
RamaGanteng14 <almaarmaa13@gmail.com>
2023-01-27 10:16:40 +0700

    update8

--- resources/views/payment/payment-add.blade.php
+++ resources/views/payment/payment-add.blade.php
@@ -50,11 +50,21 @@
                 </div>
                 <div class="form-group">
                   <label for="persentase">Amount</label>
-                  <input type="text" class="form-control" id="persentase" name="persentase">
+                  <input type="text" class="form-control" id="amount" name="amount_payment">
                 </div>
                 <div class="form-group">
                   <label for="date">Date</label>
-                  <input type="date" class="form-control" id="date" name="date" >
+                  <input type="date" class="form-control" id="date" name="payment_date" >
+                </div>
+                <div class="form-group">
+                  <label for="name">Payment Method</label>
+                  <select name="payment_method_id" id="payment_method_id" class="form-control " >
+    
+                    @foreach ($payments as $item)
+                    <option value="{{ $item->id}}">{{ $item->method}}</option>
+                    @endforeach
+                    
+                </select>
                 </div>
                 <div class="form-group">
                   <label for="date">Payment To</label>
