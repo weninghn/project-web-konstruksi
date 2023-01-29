df31a5d3584e81f77120c82b9e37e2262f223af2
febianaayurach <rachmafebiana@gmail.com>
2023-01-29 20:10:35 +0700

    kedua

--- resources/views/project/project.blade.php
+++ resources/views/project/project.blade.php
@@ -88,7 +88,7 @@ Project
                                   </td>
                                   <td>
                                   <a href="/edit/{{ $project->id }}">Edit</button> | 
-                                    <a href="/delete/{{ $project->id }}" data-name="{{ $project->name }}">Delete</a>
+                                  <a href="/delete/{{ $project->id }}" data-name="{{ $project->name }}">Delete</a>
                                   </td>
                     </tr>
                   </tbody>
