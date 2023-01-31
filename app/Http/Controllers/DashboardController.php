20e244076f6098fbdd50bfae11b22119c2b7cfb9
RamaGanteng14 <almaarmaa13@gmail.com>
2023-01-31 09:38:02 +0700

    u13

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,12 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Offer;
+use App\Models\Progres;
+use App\Models\Project;
+use App\Models\Payment;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('admin.dashboard');
+        $offercount = Offer::count();
+        $projectcount = Project::count();
+        $progrescount = Progres::count();
+        $paymentcount = Payment::count();
+        return view('admin.dashboard',['offer_count'=>$offercount, 'project_count' => $projectcount, 'progres_count' => $progrescount, 'payment_count' => $progrescount]);
     }
 }
